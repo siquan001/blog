91ed3a5d400b7dda5865a0fef30d8b590e1f8131
siquan001 <chensiquan2022@163.com>
2023-10-02 15:07:03 +0800

    update

---
+++ build.js
@@ -0,0 +1,206 @@
+const fs = require('fs');
+const path = require('path');
+const jsyaml = require('js-yaml');
+const {Marked} = require('marked');
+const hljs=require('highlight.js');
+const {markedHighlight}=require('marked-highlight');
+
+const marked=new Marked(
+  markedHighlight({
+    langPrefix: 'hljs language-',
+    highlight(code, lang) {
+      const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+      return hljs.highlight(code, { language }).value;
+    }
+  })
+)
+
+const config = {
+  post: {
+    pageshowcount: 10,
+    toppershowcount: 3,
+  },
+  t: {
+    pageshowcount: 10,
+    toppershowcount: 3,
+  },
+  archives:{
+    post:[],
+    t:[],
+  }
+}
+
+function chulipost() {
+  // 获取所有post
+  let allpost = [];
+  let allpostcontent = {};
+
+  let posts = fs.readdirSync(path.join(__dirname, '/source/posts'));
+
+  posts.forEach(function (item) {
+    if (path.extname(item).indexOf('md') != -1) {
+      let content = fs.readFileSync(path.join(__dirname, '/source/posts', item));
+      content = content.toString();
+      let info = (/---(.*?)---/sg).exec(content)[1];
+      info = jsyaml.load(info);
+      content = content.replace(/---(.*?)---/sg, '');
+      allpost.push(info);
+      allpostcontent[info.id] = content;
+    }
+  })
+
+  allpost.sort((a, b) => {
+    var ad = new Date(a.time).getTime();
+    var bd = new Date(a.time).getTime();
+    var aq = !!a.top, bq = !!b.top;
+    if (aq && !bq) {
+      return 1;
+    } else if (!aq && bq) {
+      return -1;
+    } else {
+      return ad - bd;
+    }
+  })
+
+  fs.writeFileSync(path.join(__dirname, 'all_post.json'), JSON.stringify(allpost));
+
+  for (let id in allpostcontent) {
+    if(config.archives.post.indexOf(id)!=-1) continue;
+    try {
+      var yc = fs.readFileSync(path.join(__dirname, 'posts', id + '.md'));
+      yc = yc.toString();
+      if (yc != allpostcontent[id]) {
+        fs.writeFileSync(path.join(__dirname, 'posts', id + '.md'), allpostcontent[id])
+        fs.writeFileSync(path.join(__dirname, 'posts', id + '.html'), marked.parse(allpostcontent[id]))
+      }
+    } catch (e) {
+      fs.writeFileSync(path.join(__dirname, 'posts', id + '.md'), allpostcontent[id])
+      fs.writeFileSync(path.join(__dirname, 'posts', id + '.html'), marked.parse(allpostcontent[id]))
+    }
+  }
+  
+  allpost.forEach(function(item){
+    fs.writeFileSync(path.join(__dirname, 'posts', item.id + '.json'), JSON.stringify(item));
+  })
+
+  fs.readdirSync(path.join(__dirname, 'post_comment')).forEach(function (item) {
+    fs.unlinkSync(path.join(__dirname, 'post_comment', item));
+  });
+
+  let post_page_total = Math.floor(allpost.length / config.post.pageshowcount) + 1;
+
+  for (var i = 0; i < post_page_total; i++) {
+    let ob = {
+      data: [],
+      total: post_page_total - 1
+    };
+    for (var j = i * config.post.pageshowcount; j < Math.min(allpost.length, (i + 1) * config.post.pageshowcount); j++) {
+      ob.data.push(allpost[j]);
+    }
+    fs.writeFileSync(path.join(__dirname, 'post_comment', i + '.json'), JSON.stringify(ob));
+  }
+
+  let topperob = [];
+  for (var i = 0; i < Math.min(allpost.length, config.post.toppershowcount); i++) {
+    topperob.push(allpost[i]);
+  }
+  fs.writeFileSync(path.join(__dirname, 'topper_post.json'), JSON.stringify(topperob));
+
+  var tags={};
+  allpost.forEach(function(item){
+    if(item.tags){
+      item.tags.forEach((tag)=>{
+        if(tags[tag]){
+          tags[tag].push(item);
+        }else{
+          tags[tag]=[item];
+        }
+      })
+    }
+  })
+
+  for(let k in tags){
+    fs.writeFileSync(path.join(__dirname, 'tags', k + '.json'), JSON.stringify(tags[k]));
+  }
+  fs.writeFileSync(path.join(__dirname, './tags.json'), JSON.stringify(Object.keys(tags)));
+}
+
+chulipost();
+
+function chulit(){
+  var allt=[];
+  var alltcontent={};
+  let ts = fs.readdirSync(path.join(__dirname, '/source/ts'));
+
+  ts.forEach(function (item) {
+    if (path.extname(item).indexOf('md') != -1) {
+      let content = fs.readFileSync(path.join(__dirname, '/source/ts', item));
+      content = content.toString();
+      let info = (/---(.*?)---/sg).exec(content)[1];
+      info = jsyaml.load(info);
+      content = content.replace(/---(.*?)---/sg, '');
+      info.desc=content.substring(0,50);
+      allt.push(info);
+      alltcontent[info.id] = content;
+    }
+  })
+
+  allt.sort((a, b) => {
+    var ad = new Date(a.time).getTime();
+    var bd = new Date(a.time).getTime();
+    var aq = !!a.top, bq = !!b.top;
+    if (aq && !bq) {
+      return 1;
+    } else if (!aq && bq) {
+      return -1;
+    } else {
+      return ad - bd;
+    }
+  })
+
+  fs.writeFileSync(path.join(__dirname, 'all_t.json'), JSON.stringify(allt));
+
+  for (let id in alltcontent) {
+    try {
+      var yc = fs.readFileSync(path.join(__dirname, 'ts', id + '.md'));
+      yc = yc.toString();
+      if (yc != alltcontent[id]) {
+        fs.writeFileSync(path.join(__dirname, 'ts', id + '.md'), alltcontent[id])
+        fs.writeFileSync(path.join(__dirname, 'ts', id + '.html'), marked.parse(alltcontent[id]))
+      }
+    } catch (e) {
+      fs.writeFileSync(path.join(__dirname, 'ts', id + '.md'), alltcontent[id])
+      fs.writeFileSync(path.join(__dirname, 'ts', id + '.html'), marked.parse(alltcontent[id]))
+    }
+  }
+
+  allt.forEach(function(item){
+    fs.writeFileSync(path.join(__dirname, 'ts', item.id + '.json'), JSON.stringify(item));
+  })
+
+
+  fs.readdirSync(path.join(__dirname, 't_comment')).forEach(function (item) {
+    fs.unlinkSync(path.join(__dirname, 't_comment', item));
+  });
+
+  let t_page_total = Math.floor(allt.length / config.t.pageshowcount) + 1;
+
+  for (var i = 0; i < t_page_total; i++) {
+    let ob = {
+      data: [],
+      total: t_page_total - 1
+    };
+    for (var j = i * config.t.pageshowcount; j < Math.min(allt.length, (i + 1) * config.t.pageshowcount); j++) {
+      ob.data.push(allt[j]);
+    }
+    fs.writeFileSync(path.join(__dirname, 't_comment', i + '.json'), JSON.stringify(ob));
+  }
+
+  let topperob = [];
+  for (var i = 0; i < Math.min(allt.length, config.t.toppershowcount); i++) {
+    topperob.push(allt[i]);
+  }
+  fs.writeFileSync(path.join(__dirname, 'topper_t.json'), JSON.stringify(topperob));
+}
+
+chulit();
