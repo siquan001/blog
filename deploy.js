4cfc0396f5d8958e24588ffea0c15f55f247b611
siquan001 <chensiquan2022@163.com>
2023-10-05 10:14:54 +0800

    update

--- deploy.js
+++ deploy.js
@@ -10,7 +10,7 @@ shell.exec('git add .');
 console.log('[DEPLOY] git added');
 // git commit
 shell.exec('git commit -m "update"');
-console.log('[DEPLOY] git commit');
+console.log('[DEPLOY] git commited');
 // git push
 shell.exec('git push');
-console.log('[DEPLOY] git push');
+console.log('[DEPLOY] git pushed');
